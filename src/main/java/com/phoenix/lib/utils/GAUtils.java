6147c76ff637df3ca44229cbffd4f8d90cfebd4c
Dylan Jones <dylan>
2014-09-14 11:41:58 -0600

    Added the Google Analytics Dialog. Also, abstracted out the Google Analytics calls to reflect if it has been disable by the user.

---
+++ src/main/java/com/phoenix/lib/utils/GAUtils.java
@@ -0,0 +1,49 @@
+package com.phoenix.lib.utils;
+
+import android.util.Log;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+import com.phoenix.lib.R;
+
+/**
+ * Created by Dylan on 9/14/2014.
+ */
+public class GAUtils {
+
+    public synchronized Tracker getTracker(TrackerName trackerId) {
+        if (getAnalyticsId() == 0) {
+            Log.e(getApplicationName(), "Trying to get Analytics tracker without an id");
+            return null;
+        }
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker(getAnalyticsId())
+                    : analytics.newTracker(R.xml.global_tracker);
+            t.enableAdvertisingIdCollection(true);
+            mTrackers.put(trackerId, t);
+
+        }
+        return mTrackers.get(trackerId);
+    }
+
+    /**
+     * Enum used to identify the tracker that needs to be used for tracking.
+     * <p/>
+     * A single tracker is usually enough for most purposes. In case you do need multiple trackers,
+     * storing them all in Application object helps ensure that they are created only once per
+     * application instance.
+     */
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this
+        // app.
+        GLOBAL_TRACKER, // Tracker used by all the
+        // apps from a company.
+        // eg: roll-up tracking.
+        ECOMMERCE_TRACKER, // Tracker used by all
+        // ecommerce
+        // transactions from a
+        // company.
+    }
+}
